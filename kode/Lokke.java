b0548ad1e583a4f5c19371c45ffca70d819d5b64
JarlTuxen <jart@kea.dk>
2018-09-28 11:24:15 +0200

    primtal

--- kode/Lokke.java
+++ kode/Lokke.java
@@ -21,11 +21,32 @@ public class Lokke
       
       //typisk mønster
       //gør noget indtil betingelse er opfyldt
-      boolean koerLokke = true;
-      while (koerLokke)
-      {
+      // boolean koerLokke = true;
+      // while (koerLokke)
+      // {
          //det vi gør undervejs
          //if (et eller andet) { koerLokke = false; }
+      // }
+      
+      //check primtal
+      System.out.println(113 + ": " + primtal(113));
+      System.out.println(117 + ": " + primtal(117));
+   }
+   
+   public static boolean primtal(int kandidat)
+   {
+      int i=2;
+      while (kandidat % i != 0)
+      {
+         i++;
+      }
+      if (i==kandidat)
+      {
+         return true;
+      }
+      else
+      {
+         return false;
       }
    }
 
