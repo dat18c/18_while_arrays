8c933d9ab859f2b924210887a189bcadadd3eab1
JarlTuxen <jart@kea.dk>
2018-09-28 09:26:10 +0200

    leg med lighedstegn

---
+++ September.java
@@ -0,0 +1,17 @@
+public class September{
+
+   public static void main(String[] args){
+      int a = 7;
+      int b = 9;
+      if (a == b)
+      {
+         System.out.println(a + " " + b);
+      }
+      a = b;
+      if (a == b)
+      {
+         System.out.println(a + " " + b);
+      }
+   }
+
+}
